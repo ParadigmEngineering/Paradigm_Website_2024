9a4b8498105d85fe2b64539fe442ba4816791586
liam-earle <38230039+liam-earle@users.noreply.github.com>
2023-10-09 17:46:51 -0230

    Initial Content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,png}'],
 	theme: {
 		extend: {
-			fontFamily:{
+			fontFamily: {
 				techmono: ['ShareTechMono', 'monospace'],
 				tech: ['ShareTech', 'sans-serif'],
 				russo: ['RussoOne', 'sans-serif'],
-				bebas: ['BebasNeue', 'cursive']
-			}
+				bebas: ['BebasNeue', 'cursive'],
+				roboto: ['Roboto', 'roboto']
+			},
+			animation: {
+				'spin-slow': 'spin 10s linear infinite'
+			},
 		},
 	},
 	plugins: [],
